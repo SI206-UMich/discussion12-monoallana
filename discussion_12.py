e6e6d50665010321b83da5026c93b0b3045b262e
monoallana <81531433+monoallana@users.noreply.github.com>
2022-11-30 14:04:24 -0500

    struggle bus

--- discussion_12.py
+++ discussion_12.py
@@ -16,9 +16,11 @@ def setUpDatabase(db_name):
 # TASK 1
 # CREATE TABLE FOR EMPLOYEE INFORMATION IN DATABASE AND ADD INFORMATION
 def create_employee_table(cur, conn):
-    pass
+    # specify primary key
+    cur.execute('DROP TABLE IF EXISTS employees (employee_id INTEGER PRIMARY KEY, first_name TEXT, last_name TEXT, hire_date TEXT, job_id INT, salary INT)') 
+    conn.commit()
 
-# ADD EMPLOYEE'S INFORMTION TO THE TABLE
+# ADD EMPLOYEE'S INFORMATION TO THE TABLE
 
 def add_employee(filename, cur, conn):
     #load .json file and read job data
@@ -27,20 +29,62 @@ def add_employee(filename, cur, conn):
     file_data = f.read()
     f.close()
     # THE REST IS UP TO YOU
-    pass
+    employee_data = json.loads(file_data)
+    for item in employee_data:
+        employee_id = int(item['employee_id'])
+        first_name = item['first_name']
+        last_name = item['last_name']
+        hire_date = item['hire_date']
+        job_id = int(item['job_id'])
+        salary = int(item['salary'])
+        cur.execute('INSERT INTO employees (employee_id, first_name, last_name, hire_date, job_id, salary) VALUES (?,?,?,?,?,?)' (employee_id, first_name, last_name, hire_date, job_id, salary))
+    conn.committ()
 
 # TASK 2: GET JOB AND HIRE_DATE INFORMATION
 def job_and_hire_date(cur, conn):
-    pass
+    cur.execute('SELECT employees.hire_date, jobs.job_title FROM employees JOIN jobs ON employees.job_id = jobs.job_id')
+    job_hire_date = cur.fetchall()
+    print(job_hire_date)
+    conn.commit()
 
+    sorted_job_hire_date = sorted(job_hire_date, key=lambda x:x[0])
+    print(sorted_job_hire_date)
+    conn.commit()
 # TASK 3: IDENTIFY PROBLEMATIC SALARY DATA
 # Apply JOIN clause to match individual employees
 def problematic_salary(cur, conn):
-    pass
+    cur.execute('SELECT employees.first_name, employees.last_name FROM employees JOIN jobs ON employees.job_id = jobs.job_id WHERE employees.salary > jobs.max_salary OR employees.salary < jobs.min_salary')
+    invalid = cur.fetchall()
+    print(invalid)
+    conn.commit()
+    return invalid
 
 # TASK 4: VISUALIZATION
 def visualization_salary_data(cur, conn):
-    pass
+    cur.execute('SELECT employees.salary, jobs.job_title FROM employees JOIN jobs ON employees.jobs_id = jobs.job_id')
+    salary_data = cur.fetchall()
+    conn.commit()
+    print(salary_data)
+
+    salary_list = []
+    job_list = []
+    for item in salary_data:
+        salary_list.append(item[0])
+        job_list.append(item[1])
+
+    plt.figure()
+    plt.scatter(job_list, salary_list)
+
+    cur.execute('SELECT jobs.job_title, jobs.max_salary, jobs.min_salary FROM jobs')
+    job_data = cur.fetchall()
+
+    salary_list = []
+    job_list = []
+    for item in job_data:
+
+    plt.xticks(rotation = 45)
+    plt.tight_layout()
+    plt.show()
 
 class TestDiscussion12(unittest.TestCase):
     def setUp(self) -> None:
@@ -75,6 +119,8 @@ def main():
 
     wrong_salary = (problematic_salary(cur, conn))
     print(wrong_salary)
+
+    visualization_salary_data(cur, conn)
 
 if __name__ == "__main__":
     main()
